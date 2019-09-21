d54dcd326e11bdbd4c9107c1ac98a88d82d41089
Erik Martinez <ejm20658@gmail.com>
2019-09-21 16:43:57 -0600

    prepping for wk2 homepage

--- web/common/header.php
+++ web/common/header.php
@@ -1,7 +1,8 @@
 <header>
   <nav>
     <ul>
-      <li><a href="../hello.html"> Assignment Portal</a></li>
+     <li>Home</li>
+     <li><a href="../hello.html">Assignment Portal</a></li>
     </ul>
   </nav>
 </header>
