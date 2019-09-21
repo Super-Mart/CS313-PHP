52bd5aa749a46e24d82211c134bf799e75f1ce19
Erik Martinez <ejm20658@gmail.com>
2019-09-21 16:05:17 -0600

    prepping for wk2 homepage

---
+++ web/common/header.php
@@ -0,0 +1,7 @@
+<header>
+  <nav>
+    <ul>
+      <li><a href="../hello.html"> Assignment Portal</a></li>
+    </ul>
+  </nav>
+</header>
--- web/index.php
+++ web/index.php
@@ -4,11 +4,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="author" content="Erik Martinez">
   <title>About Me</title>
 </head>
 <body>
+  <?php 
+    include('./common/header.php'); 
+    ?>
   <main>
-  <?php phpinfo();?>
+  <?php 
+    phpinfo();
+    ?>
   </main>
 </body>
 </html>
