8c6182f54becbcd1f30e808dcecd45de36c37d3f
Erik Martinez <ejm20658@gmail.com>
2019-09-26 10:28:39 -0600

    fix

--- web/common/footer.php
+++ web/common/footer.php
@@ -2,5 +2,5 @@
    <p> <?php 
     date_default_timezone_set('UTC');
     echo date('l, jS  F Y h:i A');
-    ?> | Thanks to Tania for the Inspiration!</p>| 
+    ?> | Thanks to Tania for the Inspiration!</p>
 </footer>
