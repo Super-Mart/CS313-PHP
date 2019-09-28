f737a3daaad20aaf9ca0ab70aa7568bb6ba1654b
Erik Martinez <ejm20658@gmail.com>
2019-09-27 19:40:49 -0600

    ewwe

--- web/team_assignments/03Teach/welcome_post.php
+++ web/team_assignments/03Teach/welcome_post.php
@@ -6,10 +6,21 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Result</title>
 </head>
-    <!-- <p> Welcome <?php echo $_POST["name"]; ?></p>
-    <p> Your email address is:<?php echo $_POST["email"]; ?></p>
-    <p> Your Major is: <?php echo $_POST["major"]; ?> </p>
-    <p> Your Comment: <?php echo $_POST["comments"]; ?> </p> -->
-    <?php echo $_POST["name"]; ?>
+
+    <?php 
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $major = $_POST["major"];
+    $comments = $_POST["comments"];
+
+    echo "Hi, ". $name . "<br>";
+    echo "Your email address: ". $email ."<br>";
+    echo "Your major is: ". $major ."<br>";
+    echo "Your posted comment: ". $comments ."<br>";
+
+    
+    ?>
+
 </body>
 </html>
