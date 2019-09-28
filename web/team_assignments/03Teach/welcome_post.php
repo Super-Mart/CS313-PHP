3e6a097c68cc2e5ce35b2120cbb08f3b52811e90
Erik Martinez <ejm20658@gmail.com>
2019-09-27 19:53:43 -0600

    hdhd

--- web/team_assignments/03Teach/welcome_post.php
+++ web/team_assignments/03Teach/welcome_post.php
@@ -13,13 +13,12 @@
     $email = $_POST["email"];
     $major = $_POST["major"];
     $comments = $_POST["comments"];
-
-    echo "<p>Hi, ". $name . "</p><br>";
+    $visted = $_POST["visted"];
+    echo "<p>Hi, ". $name . "</p>";
     echo "<a href='mailto:" . $email . "'>Your Email</a><br>";
     echo "Your major is: ". $major ."<br>";
     echo "Your posted comment: ". $comments ."<br>";
-
-    
+    echo "Places you have visited: ". $visted;    
     ?>
 
 </body>
