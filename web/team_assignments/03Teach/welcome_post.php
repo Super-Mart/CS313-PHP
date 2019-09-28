5dcf65bc6f84761b5588d82b43f7a1930bb8b95c
Erik Martinez <ejm20658@gmail.com>
2019-09-27 19:35:35 -0600

    trttr

--- web/team_assignments/03Teach/welcome_post.php
+++ web/team_assignments/03Teach/welcome_post.php
@@ -6,17 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Result</title>
 </head>
-<body>
-<?php
-
-// name attribute of the input field goes inside the 
-// square brackets of $_GET superglobal
-$name = $_POST["name"];
-$email = $_POST["email"];
-
-echo "Your name is ". $name . " and you are ". $email . " years old".
-
-?>
-    
+    <!-- <p> Welcome <?php echo $_POST["name"]; ?></p>
+    <p> Your email address is:<?php echo $_POST["email"]; ?></p>
+    <p> Your Major is: <?php echo $_POST["major"]; ?> </p>
+    <p> Your Comment: <?php echo $_POST["comments"]; ?> </p> -->
+    <?php echo $_POST["name"]; ?>
 </body>
 </html>
