95a0054296855c00dc03635f2df6045283b88eba
Erik Martinez <ejm20658@gmail.com>
2019-10-03 16:31:31 -0600

    sup

--- web/prove/03Prove/nav.php
+++ web/prove/03Prove/nav.php
@@ -1,4 +1,4 @@
-<nav>
+<!-- <nav>
     <ul class="nav nav-pills nav-fill">
         <li class="nav-item">
             <a class="nav-link" href="shopping.php">Home</a>
@@ -10,4 +10,24 @@
             <a class="nav-link" href="checkout.php">Checkout</a>
         </li>
     </ul>
+</nav> -->
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="shopping.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="order.php">View Order</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="checkout.php">Checkout</a>
+            </li>
+        </ul>
+    </div>
 </nav>
