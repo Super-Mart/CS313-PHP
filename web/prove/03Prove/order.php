9e29ced0b04bcfba88002f7495e9ab51c3dbd63a
Erik Martinez <ejm20658@gmail.com>
2019-10-03 18:31:26 -0600

    sup

--- web/prove/03Prove/order.php
+++ web/prove/03Prove/order.php
@@ -62,11 +62,13 @@ require 'start.php'
 
 	<table>
 		<tr>
-			<td colspan="5"><a href="?reset=true"><button type="button" class="btn-sm btn-danger">Clear</button></a></td>
+			<td colspan="5"><a href="?reset=true"><button type="submit" class="btn-sm btn-danger" onclick="toast()">Clear</button></a></td>
 			<td colspan="5"><a href="shopping.php"><button type="button" class="btn-sm btn-success">Keep Shopping</button></a></td>
 			<td colspan="5"><a href="checkout.php"><button type="button" class="btn-sm btn-primary">Check Out</button></a></td>
 		</tr>
 	</table>
+	<div id="snackbar">Cart Cleared</div>
+
 
 	<footer class="col-md-12">
 
@@ -74,6 +76,7 @@ require 'start.php'
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="./scripts/toast.js"></script>
 </body>
 
 </html>
