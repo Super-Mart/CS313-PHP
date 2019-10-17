0a9c9e5a29a6de4725480d15de9aa95811ee753a
Erik Martinez <ejm20658@gmail.com>
2019-10-17 11:01:28 -0600

    push

--- web/notes_project/index.php
+++ web/notes_project/index.php
@@ -25,9 +25,9 @@ $db = get_db();
         // The variable "row" now holds the complete record for that
         // row, and we can access the different values based on their
         // name
-        $user_id = $row['user_id'];
-        $category_id = $row['category_id'];
-        $created_at = $row['created_at'];
+        $user_id = $row['userId'];
+        $category_id = $row['categoryId'];
+        $created_at = $row['created'];
         $note_title = $row['note_title'];
         $note_text = $row['note_text'];
 
