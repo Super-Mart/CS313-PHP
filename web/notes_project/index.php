cf84bd28c69a3a85495ab79e1365a9ad176b9d19
Erik Martinez <ejm20658@gmail.com>
2019-10-18 11:44:38 -0600

    push

--- web/notes_project/index.php
+++ web/notes_project/index.php
@@ -6,7 +6,9 @@ include("./library/functions.php");
 
 <head>
     <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat+Subrayada:400,700|Montserrat:400,600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Notes</title>
 </head>
 
