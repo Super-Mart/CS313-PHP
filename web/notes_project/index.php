7306cb677d96a7c81270cd82fd2055f527e64ff0
Erik Martinez <ejm20658@gmail.com>
2019-10-18 11:28:04 -0600

    push

--- web/notes_project/index.php
+++ web/notes_project/index.php
@@ -11,53 +11,29 @@ include("./library/functions.php");
 </head>
 
 <body>
-    <h1>Notes</h1>
-    <div class="c_date">
-        <p>Today's Date</p>
-        <h3><?php echo date('l, M d, Y'); ?>
-            <?php echo date('h:i A'); ?></h3>
-    </div>
-    <!-- <ul class="event-list">
-        
-    </ul> -->
-    <?php
+    <main class="jumbotron-fluid">
+        <h1>Notes</h1>
+        <div class="c_date">
+            <p>Today's Date</p>
+            <h3><?php echo date('l, M d, Y'); ?>
+                <?php echo date('h:i A'); ?></h3>
+        </div>
 
-    // $statement = $db->prepare("SELECT * FROM note");
-    // $statement->execute();
-    // Go through each result
-    // while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-    // The variable "row" now holds the complete record for that
-    // row, and we can access the different values based on their
-    // name
-    // $userId = $row['userId'];
-    // $categoryId = $row['categoryId'];
-    // $created = $row['created'];
-    // $note_title = $row['note_title'];
-    // $note_text = $row['note_text'];
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">Date Added</th>
+                    <th scope="col">Note Description</th>
+                </tr>
+            </thead>
+            <tbody>
 
-    //     $noteId = $row['noteId'];
-    //     $note_description = $row['note_description'];
-    //     $date_added = $row['date_added'];
+                <?php load_notes(); ?>
 
-    //     echo "<p>$date_added<p>";
-
-    //     echo "<p>$note_description<p>";
-    // }
-    ?>
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th scope="col">Date Added</th>
-                <th scope="col">Note Description</th>
-            </tr>
-        </thead>
-        <tbody>
-
-            <?php load_notes(); ?>
-
-        </tbody>
-    </table>
-    <!-- <a href="./simple-notes-app-using-javascript-and-phpmysql/index.php" target="_blank" rel="noopener noreferrer">Link</a> -->
+            </tbody>
+        </table>
+        <!-- <a href="./simple-notes-app-using-javascript-and-phpmysql/index.php" target="_blank" rel="noopener noreferrer">Link</a> -->
+    </main>
 </body>
 
 </html>
