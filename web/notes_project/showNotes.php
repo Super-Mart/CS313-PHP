9aeaedd92ebe8f72e863e4e40671f11ec68c0d7d
Erik Martinez <ejm20658@gmail.com>
2019-10-22 12:54:19 -0600

    hehe

--- web/notes_project/showNotes.php
+++ web/notes_project/showNotes.php
@@ -48,8 +48,40 @@ include("./library/dbConnect.php");
                     ?>
                 </h3>
             </div>
+            <?php
+            try {
 
-            <table class="table table-hover">
+                // prepare the statement
+                $statement = $db->prepare('SELECT notesId, notesTitle, notesContent FROM notes');
+                $statement->execute();
+
+                // Go through each result
+                while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                    echo '<p>';
+                    echo '<strong>' . $row['notesTitle'] . ' ' . $row['notesContent'];
+                    echo '<br />';
+                    echo 'Category: ';
+
+                    $stmtCategory = $db->prepare('SELECT categoryName FROM notesCategory nc'
+                        . ' INNER JOIN notes_notesCategory nnc ON nnc.notesCategoryId = nc.notesCategoryId'
+                        . ' WHERE nnc.notesId = :notesId');
+
+                    $stmtCategory->bindValue(':notesId', $row['notesId']);
+                    $stmtCategory->execute();
+
+                    while ($categoryRow = $stmtCategory->fetch(PDO::FETCH_ASSOC)) {
+                        echo $categoryRow['categoryName'] . ' ';
+                    }
+
+                    echo '</p>';
+                }
+            } catch (PDOException $ex) {
+                echo "Error with DB. Details: $ex";
+                die();
+            }
+            ?>
+
+            <!-- <table class="table table-hover">
                 <thead>
                     <tr>
                         <th scope="col">Note Title</th>
@@ -60,41 +92,10 @@ include("./library/dbConnect.php");
                 <tbody>
 
                 </tbody>
-            </table>
+            </table> -->
             <button><a href="./newEntry.php">Add New Note</a></button>
         </div>
-        <?php
-        try {
 
-            // prepare the statement
-            $statement = $db->prepare('SELECT notesId, notesTitle, notesContent FROM notes');
-            $statement->execute();
-
-            // Go through each result
-            while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-                echo '<p>';
-                echo '<strong>' . $row['notesTitle'] . ' ' . $row['notesContent'];
-                echo '<br />';
-                echo 'Category: ';
-
-                $stmtCategory = $db->prepare('SELECT categoryName FROM notesCategory nc'
-                    . ' INNER JOIN notes_notesCategory nnc ON nnc.notesCategoryId = nc.notesCategoryId'
-                    . ' WHERE nnc.notesId = :notesId');
-
-                $stmtCategory->bindValue(':notesId', $row['notesId']);
-                $stmtCategory->execute();
-
-                while ($categoryRow = $stmtCategory->fetch(PDO::FETCH_ASSOC)) {
-                    echo $categoryRow['categoryName'] . ' ';
-                }
-
-                echo '</p>';
-            }
-        } catch (PDOException $ex) {
-            echo "Error with DB. Details: $ex";
-            die();
-        }
-        ?>
     </main>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" i ntegrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
