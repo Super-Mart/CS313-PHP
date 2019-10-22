5863dfb4c0b55b138b47b3a3a30968e1a2e77aaf
Erik Martinez <ejm20658@gmail.com>
2019-10-22 12:42:36 -0600

    hehe

--- web/notes_project/library/functions.php
+++ web/notes_project/library/functions.php
@@ -19,27 +19,32 @@ function load_notes()
 
 function load_notes_category()
 {
-    global $db;
-    // prepare the statement
-    $statement = $db->prepare('SELECT * FROM notes');
-    $statement->execute();
-    // Go through each result
-    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-        $notesId = $row['notesId'];
-        $notesTitle = $row['notesTitle'];
-        $notesContent = $row['notesContent'];
-        echo '<tr class="table-secondary"><td>' . $notesTitle . '</td>';
-        echo '<td>' . $notesContent . '</td>';
-        echo '<td>';
-        $stmtCategory = $db->prepare('SELECT categoryName FROM notesCategory nc'
-            . ' INNER JOIN notes_notesCategory nnc ON nnc.notesCategoryId = nc.notesCategoryId'
-            . ' WHERE nnc.notesId = :notesId');
+    try {
+        global $db;
+        // prepare the statement
+        $statement = $db->prepare('SELECT * FROM notes');
+        $statement->execute();
+        // Go through each result
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+            $notesId = $row['notesId'];
+            $notesTitle = $row['notesTitle'];
+            $notesContent = $row['notesContent'];
+            echo '<tr class="table-secondary"><td>' . $notesTitle . '</td>';
+            echo '<td>' . $notesContent . '</td>';
+            echo '<td></tr>';
+            // $stmtCategory = $db->prepare('SELECT categoryName FROM notesCategory nc'
+            //     . ' INNER JOIN notes_notesCategory nnc ON nnc.notesCategoryId = nc.notesCategoryId'
+            //     . ' WHERE nnc.notesId = :notesId');
 
-        $stmtCategory->bindValue(':notesId', $notesId);
-        $stmtCategory->execute();
+            // $stmtCategory->bindValue(':notesId', $notesId);
+            // $stmtCategory->execute();
 
-        while ($categoryRow = $stmtCategory->fetch(PDO::FETCH_ASSOC)) {
-            echo $categoryRow['categoryName'] . '</td></tr>';
+            // while ($categoryRow = $stmtCategory->fetch(PDO::FETCH_ASSOC)) {
+            //     echo $categoryRow['categoryName'] . '</td></tr>';
+            // }
         }
+    } catch (PDOException $ex) {
+        echo "Error with DB. Details: $ex";
+        die();
     }
 }
