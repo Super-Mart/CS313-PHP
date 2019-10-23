acf35897ed4a1488836efa244d17402e0d336713
Erik Martinez <ejm20658@gmail.com>
2019-10-23 12:28:07 -0600

    tehe

--- web/notes_project/library/functions.php
+++ web/notes_project/library/functions.php
@@ -26,7 +26,7 @@ function load_notes()
 
             // Go through each topic in the result
             while ($categoryRow = $stmtCategory->fetch(PDO::FETCH_ASSOC)) {
-                echo $categoryRow['name'] . ' ';
+                echo '<td>' . $categoryRow['name'] . '</td>';
             }
 
             echo '</tr>';
