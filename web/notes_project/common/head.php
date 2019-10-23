f69f717abedbb99108fdcd767eeedf2ed66879a9
Erik Martinez <ejm20658@gmail.com>
2019-10-23 12:05:21 -0600

    tehe

---
+++ web/notes_project/common/head.php
@@ -0,0 +1,3 @@
+<link href="https://fonts.googleapis.com/css?family=Montserrat+Subrayada:400,700|Montserrat:400,600&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="./css/bootstrap.min.css">
+<link rel="stylesheet" href="./css/styles.css">
--- web/notes_project/topicEntry.php
+++ web/notes_project/topicEntry.php
@@ -10,9 +10,9 @@ $db = get_db();
 <html>
 
 <head>
-	<link href="https://fonts.googleapis.com/css?family=Montserrat+Subrayada:400,700|Montserrat:400,600&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="./css/bootstrap.min.css">
-	<link rel="stylesheet" href="./css/styles.css">
+	<?php
+	include('./common/head.php');
+	?>
 	<title>New Entry</title>
 </head>
 
