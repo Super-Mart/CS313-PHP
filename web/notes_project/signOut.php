e7eac4ceb47ee7759710b55ffaaf3afa3b063b92
Erik Martinez <ejm20658@gmail.com>
2019-10-23 19:46:16 -0600

    tehe

---
+++ web/notes_project/signOut.php
@@ -0,0 +1,16 @@
+<?php
+
+/**********************************************************
+ * File: signOut.php
+ * Author: Br. Burton
+ * 
+ * Description: Clears the username from the session if there.
+ *
+ ***********************************************************/
+
+require("./library/password.php"); // used for password hashing.
+session_start();
+unset($_SESSION['username']);
+
+header("Location: signIn.php");
+die(); // we always include a die after redirects.
