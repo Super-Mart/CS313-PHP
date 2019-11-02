dbdac36cfa90dea7ce13b99990c86cef576c47fb
Erik Martinez <ejm20658@gmail.com>
2019-11-01 23:46:24 -0600

    ..

--- web/notes_project/showNotes.php
+++ web/notes_project/showNotes.php
@@ -66,10 +66,10 @@ if (isset($_SESSION['loggedIn'])) {
 
 					</table>
 				</div>
-				<a href="./signOut.php" class="btn btn-secondary btn-sm  mr-3">Sign Out</a>
+				<a href="./signOut.php" class="btn btn-warning btn-sm  mr-3">Sign Out</a>
 				<!-- <div class="spacer"></div> -->
-				<a href="./noteEntry.php" class="btn btn-primary btn-sm  mr-3">Add New Note</a>
-				<a href="./newCat.php" class="btn btn-primary btn-sm ">Add New Category</a>
+				<a href="./noteEntry.php" class="btn btn-light btn-sm  mr-3">Add New Note</a>
+				<a href="./newCat.php" class="btn btn-light btn-sm ">Add New Category</a>
 
 			</div>
 		</div>
