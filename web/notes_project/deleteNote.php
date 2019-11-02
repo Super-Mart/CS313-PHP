290c10707961f39dc1f838d057c25b43d6111e8f
Erik Martinez <ejm20658@gmail.com>
2019-11-01 18:35:28 -0600

    ..

---
+++ web/notes_project/deleteNote.php
@@ -0,0 +1,38 @@
+<?php
+
+// get the data from the POST
+$id = $_GET['id'];
+
+
+// echo "title=$title\n";
+// echo "content=$content\n";
+
+require("./library/dbConnect.php");
+$db = get_db();
+
+try {
+
+    // We do this by preparing the query with placeholder values
+    $query = 'SELECT FROM note WHERE id = :id';
+    $statement = $db->prepare($query);
+
+    // Now we bind the values to the placeholders. This does some nice things
+    // including sanitizing the input with regard to sql commands.
+    $statement->bindValue(':id', $id);
+
+    $statement->execute();
+
+    $rowsChanged = $statement->rowCount();
+
+    $statement->closeCursor();
+} catch (Exception $ex) {
+    // Please be aware that you don't want to output the Exception message in
+    // a production environment
+    echo "Error with DB. Details: $ex";
+    die();
+}
+
+// finally, redirect them to a new page to actually show the topics
+header("Location: showNotes.php");
+
+die();
