0c0a684e46ec70bc1a9b72353e6c5a64317e7642
Erik Martinez <ejm20658@gmail.com>
2019-11-01 23:26:36 -0600

    ..

---
+++ web/notes_project/insertCat.php
@@ -0,0 +1,33 @@
+<?php
+
+// get the data from the POST
+$name = $_POST['txtCategory'];
+
+// echo "title=$title\n";
+// echo "content=$content\n";
+
+require("./library/dbConnect.php");
+$db = get_db();
+
+try {
+
+    // We do this by preparing the query with placeholder values
+    $query = 'INSERT INTO category(name) VALUES(:name)';
+    $statement = $db->prepare($query);
+
+    // Now we bind the values to the placeholders. This does some nice things
+    // including sanitizing the input with regard to sql commands.
+    $statement->bindValue(':name', $name);
+
+    $statement->execute();
+} catch (Exception $ex) {
+    // Please be aware that you don't want to output the Exception message in
+    // a production environment
+    echo "Error with DB. Details: $ex";
+    die();
+}
+
+// finally, redirect them to a new page to actually show the topics
+header("Location: showNotes.php");
+
+die();
--- web/notes_project/showNotes.php
+++ web/notes_project/showNotes.php
@@ -67,7 +67,10 @@ if (isset($_SESSION['loggedIn'])) {
 					</table>
 				</div>
 				<a href="./signOut.php" class="btn btn-secondary mr-3">Sign Out</a>
+				<div class="spacer"></div>
 				<a href="./noteEntry.php" class="btn btn-primary">Add New Note</a>
+				<a href="./noteCat.php" class="btn btn-primary">Add New Category</a>
+
 			</div>
 		</div>
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
